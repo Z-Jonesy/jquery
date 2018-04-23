2dcf2cd5ea866c96cb625d90c1dae35fea77ed0e
Jonesy <zoltan_bogdan@live.com>
2018-04-23 13:48:13 +0200

    fixing errors

--- 02/scripts01.js
+++ 02/scripts01.js
@@ -1,27 +1,28 @@
 $(function () {
     const validClass = 'is-valid';
     const invalidClass = 'is-invalid';
+
     // note: elemek összegyűjtése
-    let $button = $('button');
-    let $checkbox = $('input[type="checkbox]');
-    let $invalidFieldlist = $('div.alert-danger ul');
-    let $alert = $('div.alert-danger');
-    let successText = $('div#successText');
-    let $form = $('form');
+    let $button = $('form button');
+    let $checkbox = $('input#aszf');
     let $username = $('input#username')
     let $password = $('input#password')
+    let $passwordAgain = $('input#passwordAgain')
+    let $alert = $('div.alert');
+    let $alertFieldlist = $('div.alert ul');
+    let $successText = $('div#successText');
+    let $form = $('form');
+
 
     //note: gombokra reagálás
     $button.click(function (event) {
         event.preventDefault();
-
         let invalidFields = [];
-        $invalidFieldlist.html('');
 
         //note: mezők ellenőrzése
         //felhasználónév
         let username = $username.val();
-        if(username.length == 0){
+        if (username.length == 0) {
             // hibás kitöltés
             invalidFields.push('Felhasználónév');
             $username.removeClass(validClass);
@@ -34,7 +35,7 @@ $(function () {
 
         //jelszó
         let password = $password.val();
-        if(password <= 5){
+        if (password <= 5) {
             //hibás kitöltés
             invalidFields.push('Jelszó');
             $password.removeClass(validClass);
@@ -45,30 +46,41 @@ $(function () {
             $password.addClass(validClass);
         }
 
+        // jelszó megerősítés
+        let passwordAgain = $passwordAgain.val();
+        if (password != passwordAgain || passwordAgain.length <= 5) {
+            invalidFields.push('Jelszó megerősítés');
+            $passwordAgain.removeClass(validClass);
+            $passwordAgain.addClass(invalidClass);
+        } else {
+            $passwordAgain.removeClass(invalidClass);
+            $passwordAgain.addClass(validClass);
+        }
+
 
         // ÁsZF
         let isChecked = $checkbox.prop('checked');
         if (!isChecked) {
             invalidFields.push('ÁSZF');
-            $checkbox.removeClass('is-valid');
-            $checkbox.addClass('is-invalid');
+            $checkbox.removeClass(validClass);
+            $checkbox.addClass(invalidClass);
         } else {
-            $checkbox.removeClass('is-invalid');
-            $checkbox.addClass('is-valid');
+            $checkbox.removeClass(invalidClass);
+            $checkbox.addClass(validClass);
         }
 
         // note: sikeres - sikertelen elágazás
         if (invalidFields.length == 0) {
-            $successText.show();
             $form.hide();
             $alert.hide();
+            $successText.show();
         } else {
             // hibák kiírása
             $alert.show();
-            $invalidFieldlist.append('<li>teszt</li>')
-            $.each(function (index, field) {
+            $alertFieldlist.html('')
+            $.each(invalidFields, function (index, field) {
                 console.log(index + ': ' + field);
-                $invalidFieldlist.append('<li' + field + '</li>')
+                $alertFieldlist.append('<li>' + field + '</li>')
             });
         }
 
