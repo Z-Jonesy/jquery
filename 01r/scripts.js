b7202e51ea1b2ce148515dc0576730ec52e9cad8
Z <zoltan_bogdan@live.com>
2018-06-03 07:09:00 +0200

    jq_r1_login form

--- 01r/scripts.js
+++ 01r/scripts.js
@@ -19,13 +19,51 @@ $(function () {
         );
 
         // átlagolás a szöveg szín megállapításhoz
-        let avg = (red + green + blue)/3;
+        let avg = (red + green + blue) / 3;
         console.log(avg);
-        if (avg /255 > 0.5) {
-            button.css('color','black');
+        if (avg / 255 > 0.5) {
+            button.css('color', 'black');
         } else {
-            button.css('color','white');
+            button.css('color', 'white');
         }
 
     });
-});
+}); // gombszínező script vége
+
+// formos feladat
+$(function () {
+    // konstansok a példa kedvéért
+    const EMAIL = 'z@com';
+    const PASSWORD = 'hey';
+
+    // elemek összegyűjtése
+    let $button = $('button.btn-primary');
+    let $email = $('input[type="email"]');
+    let $password = $('input[type="password"]');
+    let $alertsuccess = $('.alert-success');
+    let $alertdanger = $('.alert-danger');
+
+    $alertdanger.hide();
+    $alertsuccess.hide();
+
+    // click eseményre default reagálás megakasztása 
+    $button.click(function (event) {
+        event.preventDefault();
+
+        // megnézzük jót írtak-e be
+        if (EMAIL === $email.val() && PASSWORD === $password.val()) {
+            $alertdanger.hide();
+            $alertsuccess.show();
+
+        } else {
+            $alertdanger.show();
+            $alertsuccess.hide();
+        }
+    });
+}); // formos feladat vége
+
+// Progress bar
+$(function () {
+
+
+}); // progress bar
